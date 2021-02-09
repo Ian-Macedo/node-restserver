36b0a1e5dc1150f9e55e675c6dbe69c1650b3028
Ian Macedo <mian15699@gmail.com>
2021-02-08 20:55:58 -0600

    RestServer - Usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+};
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es neceasario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+//Metodo para NO mostrar el campo de la contraseña en el/los JSON que se retornen en una peticion
+userSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+userSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' });
+
+module.exports = mongoose.model('Usuario', userSchema);
